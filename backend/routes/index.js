8dff3423a21b1dc70c7606a7b7a6e341c440b708
Nicholas Oxenden <nicholas.oxenden@gmail.com>
2024-07-30 19:25:11 -0600

    Adding basic error handling

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -9,72 +9,84 @@ router.get("/", (req, res) => {
 });
 
 router.post("/seed", async (req, res) => {
-  await Location.deleteMany({});
-  await Customer.deleteMany({});
-  await CustomerLog.deleteMany({});
+  try {
+    await Location.deleteMany({});
+    await Customer.deleteMany({});
+    await CustomerLog.deleteMany({});
 
-  // locations
-  for (const location of locations) {
-    await location.save();
+    // locations
+    for (const location of locations) {
+      await location.save();
+    }
+
+    // customers
+    for (const customer of customers) {
+      await customer.save();
+    }
+
+    // customer logs
+    for (const customerLog of customerLogs) {
+      await customerLog.save();
+    }
+
+    return res.status(200).json({ message: "Database seeded successfully" });
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({ message: "Error seeding database" });
   }
-
-  // customers
-  for (const customer of customers) {
-    await customer.save();
-  }
-
-  // customer logs
-  for (const customerLog of customerLogs) {
-    await customerLog.save();
-  }
-
-  return res.status(200).json({ message: "Database seeded successfully" });
 });
 
 // Assume req.body contains a locationId, startDate and endDate.
 // Assume startDate and endDate to be in ISO 8601 format.
 router.post("/opiniionTest", async (req, res) => {
-  const { locationId, startDate, endDate } = req.body;
+  try {
+    const { locationId, startDate, endDate } = req.body;
 
-  const result = await Customer.aggregate([
-    { $match: { locationId } },
-    {
-      $lookup: {
-        from: CustomerLog.collection.name,
-        localField: "customerId",
-        foreignField: "customerId",
-        as: "logs",
+    const result = await Customer.aggregate([
+      { $match: { locationId } },
+      {
+        $lookup: {
+          from: CustomerLog.collection.name,
+          localField: "customerId",
+          foreignField: "customerId",
+          as: "logs",
+        },
       },
-    },
-    {
-      $match: {
-        "logs.date": { $gte: new Date(startDate), $lte: new Date(endDate) },
+      {
+        $match: {
+          "logs.date": { $gte: new Date(startDate), $lte: new Date(endDate) },
+        },
       },
-    },
-    {
-      $group: {
-        _id: "$customerId",
-        firstName: { $first: "$firstName" },
-        lastName: { $first: "$lastName" },
-        email: { $first: "$email" },
-        phone: { $first: "$phone" },
-        logs: { $push: "$logs" },
+      {
+        $group: {
+          _id: "$customerId",
+          firstName: { $first: "$firstName" },
+          lastName: { $first: "$lastName" },
+          email: { $first: "$email" },
+          phone: { $first: "$phone" },
+          logs: { $push: "$logs" },
+        },
       },
-    },
-    {
-      $project: {
-        _id: 0,
-        customerId: "$_id",
-        firstName: 1,
-        lastName: 1,
-        email: 1,
-        phone: 1,
-        logs: 1,
+      {
+        $project: {
+          _id: 0,
+          customerId: "$_id",
+          firstName: 1,
+          lastName: 1,
+          email: 1,
+          phone: 1,
+          logs: 1,
+        },
       },
-    },
-  ]);
+    ]);
 
-  res.status(200).json(result);
+    res.status(200).json(result);
+  } catch (err) {
+    console.error(err);
+    return res
+      .status(500)
+      .json({ message: "Error retrieving opiniion test data" });
+  }
 });
 
 export default router;
